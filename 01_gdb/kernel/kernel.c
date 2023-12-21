2f25620f053708383070aa1f95069884d875a9c7
Fz Iguenfer <91503768+patime07@users.noreply.github.com>
2023-12-21 13:07:46 +0100

    Add files via upload

---
+++ 01_gdb/kernel/kernel.c
@@ -0,0 +1,29 @@
+/* This will force us to create a kernel entry function instead of jumping to kernel.c:0x00 */
+void dummy_test_entrypoint() {
+}
+// use _start instead of main, kinda like begin for arduino
+void _start() {
+	char* video_memory = (char*) 0xb8000;
+	*video_memory = 'X';
+    unsigned char a;
+    unsigned char b;
+    a = 0;
+    b = 0;
+    for( int i = 1; i < 24; ++i) {
+            for( int j = 0; j < 80; ++j) {
+            *(video_memory + i*160 + j*2) = '.';
+            }
+    }
+
+    *video_memory = 'Z';
+    for( int i = 10; i < 20; ++i) {
+            for( int j = 50; j < 60; ++j) {
+            *(video_memory + i*160 + j*2) = 'X';
+            }
+    }
+    while(1) {
+            *(video_memory+0) = b;
+            *(video_memory+1) = ++a;
+            b = b + a/255;
+    }
+}
